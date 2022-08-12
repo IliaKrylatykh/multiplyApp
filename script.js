8f2376844ea1642bb77a041304c4d7d6ab30c87e
Ilia Krylatykh <iliakrylatykh@gmail.com>
2022-08-12 22:06:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use ctrict";
+
+var score = 0;
+var header = document.getElementById("header");
+var calc = 0;
+var userAnswer = 0;
+
+let a;
+let b;
+let task;
+
+a = 1;
+b = 2;
+
+task = "What is " + a + "multiply by " + b + "?";
+
+header.innerHTML = task;
