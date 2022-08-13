07e30399aa73b827c18d79011196e19c7286685b
Ilia Krylatykh <iliakrylatykh@gmail.com>
2022-08-13 19:50:12 +0500

    commit

--- script.js
+++ script.js
@@ -3,15 +3,33 @@
 var score = 0;
 var header = document.getElementById("header");
 var calc = 0;
-var userAnswer = 0;
+var userAnswer = document.getElementById("form");
+
+const inputEl = document.getElementById("input");
 
 let a;
 let b;
-let task;
+let taskText;
 
-a = 1;
-b = 2;
+a = Math.ceil(Math.random() * 10);
+b = Math.ceil(Math.random() * 10);
+calc = a * b;
 
-task = "What is " + a + "multiply by " + b + "?";
+userAnswer.addEventListener("submit", () => {
+  const userAnswer = +inputEl.value;
+  console.log(userAnswer, typeof userAnswer);
+  if (userAnswer === calc) {
+    score++;
+  } else {
+    score--;
+  }
+  console.log(score);
+});
 
-header.innerHTML = task;
+console.log(calc);
+console.log(userAnswer);
+
+taskText = "What is " + a + " multiply by " + b + "?";
+
+header.innerHTML = taskText;
+score.innerHTML = score;
